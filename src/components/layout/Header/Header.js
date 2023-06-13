f5ae3027d60bb31070f63129b87b3da00ce2c232
springsandy <48716493+springsandy@users.noreply.github.com>
2023-06-13 15:56:35 +0900

    add home page

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header class="header">
+      <div>
+        <h1 class="loge">
+          <a href="/">Book Shoping</a>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/layout/Home/Home.js
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const Home = () => {
+  return (
+    <div class="bodyDiv">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
